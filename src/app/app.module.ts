8eff6c0dcc6f9a08f69d6ac46685913a1992364e
Mubasharyar <maw8051@rit.edu>
2023-05-16 00:09:30 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+import { AppComponent } from './app.component';
+import {PostCreateComponent} from './posts/post-create/post-create.component';
+import {PostListComponent} from './posts/post-list/post-list.component';
+import {HeaderComponent} from './header/header.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent,
+    HeaderComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+
+})
+export class PostCreateComponent{
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter<Post>();
+
+
+  onAddPost(){
+
+    const post: Post = {title: this.enteredTitle, content: this.enteredContent}
+    this.postCreated.emit(post);
+
+  }
+
+}
