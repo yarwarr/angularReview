6cb3fb2b6b1d4f2208dc55236070ee18535cb290
Mubasharyar <maw8051@rit.edu>
2023-05-16 00:24:16 -0400

    adding a different way to submit form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -13,9 +14,11 @@ export class PostCreateComponent{
   @Output() postCreated = new EventEmitter<Post>();
 
 
-  onAddPost(){
-
-    const post: Post = {title: this.enteredTitle, content: this.enteredContent}
+  onAddPost(form: NgForm){
+    if(form.invalid){
+      return;
+    }
+    const post: Post = {title: form.value.title, content: form.value.content}
     this.postCreated.emit(post);
 
   }
